c244ef52136f6b62abf80642a0c6f3e31169d203
sabjorn <info@sabjorn.net>
2019-03-20 14:39:49 -0700

    Migrates Repo to Use PiMediaSync Application
    The origian application in this repo has now been moved to [PiMediaSync](https://github.com/limbicmedia/piMediaSync)
    
    This repo now contains only the bare essentials for setting up the PiMediaSync application with:
    
    * sawmill config file
    * sawmill video file
    
    The install script has been changed to manage all installation (including setting up PiMediaSync).
    
    Additionally:
    * added lfs gitattributes (for large video file)
    * started modifying README to fit new install steps

---
+++ setup.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+## Mini-World Sawmill Installer
+# This script installs the PiMediaSync application and sets 
+# up all of the components (video and config) necessary to 
+# run the installation. 
+#
+# Note, all ENV with 'export' are used by the PiMediaSync setup script
+
+set -e
+if [ "$(whoami)" != "root" ]; then
+    echo "must be run as root."
+    exit 1G
+fi
+
+HOMEDIR="${HOMEDIR:-/root}"
+WORKDIR="${HOMEDIR}/sawmill"
+SAWMILL_VERSION="${SAWMILL_VERSION:-0.2.0}"
+
+# mini-world-sawmill repo uses git lfs, currently not available on RPI
+# so, wget the files we want
+rm -rf ${WORKDIR} # clear first
+mkdir -p ${WORKDIR}/video
+wget -P ${WORKDIR} https://raw.githubusercontent.com/limbicmedia/mini-world-sawmill-display/${SAWMILL_VERSION}/sawmill_config.py
+wget -P ${WORKDIR}/video https://github.com/limbicmedia/mini-world-sawmill-display/raw/${SAWMILL_VERSION}/video/sawmill.mov
+
+export PIHOSTNAME="miniworld-sawmill"
+
+# Setup PiMediaSync application
+SAWMILL_CONFIG_FILE="${SAWMILL_CONFIG_FILE:-sawmill_config.py}"
+export APPLICATION_FLAGS="-c${WORKDIR}/${SAWMILL_CONFIG_FILE}"
+export PIMEDIASYNC_VERSION="${PIMEDIASYNC_VERSION:-dev}"
+wget -O - https://raw.githubusercontent.com/limbicmedia/PiMediaSync/${PIMEDIASYNC_VERSION}/scripts/install.sh | bash
+
+# install video in location used by PiMediaSync
+PIMEDIASYNC_DIR="${PIMEDIASYNC_DIR:-/opt/pimediasync}" # come from export in pimediasync setup
+ln -s ${WORKDIR}/video ${PIMEDIASYNC_DIR}/video
+
+echo "Installation Finished, reboot the device."
+set +e
